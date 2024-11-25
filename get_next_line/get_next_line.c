c670fc4d97271c5b207a2cc635c39bc9ccb59111
HOUSSAM MNASFA <126343598+MNASFA@users.noreply.github.com>
2024-11-25 16:11:32 +0100

    Add files via upload

---
+++ get_next_line/get_next_line.c
@@ -0,0 +1,92 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hmnasfa <hmnasfa@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/11/23 13:50:49 by hmnasfa           #+#    #+#             */
+/*   Updated: 2024/11/25 15:40:54 by hmnasfa          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line.h"
+
+static char *extract_line(char **bank)
+{
+	char *new_line_stop;
+	char *line;
+	char *leftover;
+	size_t sub_lenght;
+
+	if (!bank || !*bank || !**bank)
+		return (NULL);
+	new_line_stop = ft_strchr(*bank, '\n');
+	if (new_line_stop)
+	{
+		sub_lenght = new_line_stop - *bank + 1;
+		line = ft_substr(*bank, 0, sub_lenght);
+		leftover = ft_strdup(new_line_stop + 1);
+		free(*bank);
+		*bank = leftover;
+	}
+	else
+	{
+		line = ft_strdup(*bank);
+		free(*bank);
+		*bank = NULL;
+	}
+	return(line);
+}
+
+char	*get_next_line(int fd)
+{
+	char *tmp_buffer;
+	static char *bank;
+	ssize_t read_bytes;
+	char *tmp;
+	
+	if (fd < 0 || BUFFER_SIZE <= 0)
+		return (NULL);
+	if (!bank)
+    	bank = ft_strdup("");
+	tmp_buffer = (char *) malloc((BUFFER_SIZE + 1) * sizeof(char));
+	if (!tmp_buffer)
+		return (NULL);
+	while (!ft_strchr(bank, '\n'))
+	{
+		read_bytes = read(fd, tmp_buffer, BUFFER_SIZE);
+		if (read_bytes <= 0)
+		{
+			if (read_bytes == 0)
+				return(extract_line(&bank));
+			free(bank);
+			bank = NULL;
+			return (NULL);
+		}
+		tmp_buffer[read_bytes] = '\0';
+		if (bank)
+			tmp = ft_strjoin(bank, tmp_buffer);
+		else
+			tmp = ft_strdup(tmp_buffer);
+		free (bank);
+		bank = tmp;
+	}
+	return(extract_line(&bank));
+}
+#include <libc.h>
+int main(void)
+{
+	
+	int fd = open("test.txt" , O_RDONLY);
+	if (fd == -1)
+		return (1);
+	
+	char *line;
+	while ((line = get_next_line(fd)) != NULL)
+	{
+		printf("%s" , line);
+		free(line);
+	}
+	return(0);
+}
