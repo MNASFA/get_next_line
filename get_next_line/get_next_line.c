666c4e2972a50b3ab3ffaaa143dda9f07ec98fab
HOUSSAM MNASFA <126343598+MNASFA@users.noreply.github.com>
2024-11-27 15:29:36 +0100

    Update get_next_line.c
    Updated code for francinette testing(free and initialize bank to null)

--- get_next_line/get_next_line.c
+++ get_next_line/get_next_line.c
@@ -3,24 +3,24 @@
 /*                                                        :::      ::::::::   */
 /*   get_next_line.c                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: marvin <marvin@student.42.fr>              +#+  +:+       +#+        */
+/*   By: hmnasfa <hmnasfa@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/23 13:50:49 by hmnasfa           #+#    #+#             */
-/*   Updated: 2024/11/25 16:46:52 by marvin           ###   ########.fr       */
+/*   Updated: 2024/11/27 15:25:46 by hmnasfa          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
-static char *extract_line(char **bank)
+static char	*extract_line(char **bank)
 {
-	char *new_line_stop;
-	char *line;
-	char *leftover;
-	size_t sub_lenght;
+	char	*new_line_stop;
+	char	*line;
+	char	*leftover;
+	size_t	sub_lenght;
 
-	if (!bank || !*bank || !**bank)
-		return (NULL);
+	if (!*bank || !**bank)
+		return (free(*bank), *bank = NULL, NULL);
 	new_line_stop = ft_strchr(*bank, '\n');
 	if (new_line_stop)
 	{
@@ -36,62 +36,50 @@ static char *extract_line(char **bank)
 		free(*bank);
 		*bank = NULL;
 	}
-	return(line);
+	return (line);
+}
+
+int	ft_help(int fd, char *tmp_buffer)
+{
+	ssize_t	read_bytes;
+	
+	read_bytes = read (fd, tmp_buffer, BUFFER_SIZE);
+	if (read_bytes <= 0)
+	{
+		if (read_bytes < 0)
+			return (-1);
+		return (0);
+	}
+	tmp_buffer[read_bytes] = '\0';
+	return (1);
 }
 
 char	*get_next_line(int fd)
 {
-	char *tmp_buffer;
-	static char *bank;
-	ssize_t read_bytes;
-	char *tmp;
-	
-	if (fd < 0 || BUFFER_SIZE <= 0)
+	static char	*bank;
+	char		*tmp_buffer;
+	char		*tmp;
+	int res;
+
+	if (fd < 0 || BUFFER_SIZE < 1 || BUFFER_SIZE > INT_MAX)
 		return (NULL);
 	if (!bank)
-    	bank = ft_strdup("");
-	tmp_buffer = (char *) malloc((BUFFER_SIZE + 1) * sizeof(char));
+		bank = ft_strdup("");
+	tmp_buffer = (char *) malloc ((BUFFER_SIZE + 1) * sizeof(char));
 	if (!tmp_buffer)
-		return (NULL);
+		return (free(bank), NULL);
 	while (!ft_strchr(bank, '\n'))
 	{
-		read_bytes = read(fd, tmp_buffer, BUFFER_SIZE);
-		if (read_bytes <= 0)
-		{
-			if (read_bytes < 0)
-			{
-				free(tmp_buffer);
-				return (NULL);
-			}
-			if (read_bytes == 0)
-				break;
-		}
-		tmp_buffer[read_bytes] = '\0';
+		res = ft_help(fd , tmp_buffer);
+		if ((res = ft_help(fd , tmp_buffer)) == -1)
+			return(free(tmp_buffer), free(bank), bank = NULL, NULL);
+		else if ((res = ft_help(fd , tmp_buffer)) == 0)
+			break;
 		tmp = ft_strjoin(bank, tmp_buffer);
 		if (!tmp)
-		{
-			free(tmp_buffer);
-			return(NULL);
-		}
+			return (free(tmp_buffer), free(bank), bank = NULL, NULL);
 		free (bank);
 		bank = tmp;
 	}
-	free(tmp_buffer);
-	return(extract_line(&bank));
-}
-#include <fcntl.h>
-int main(void)
-{
-	
-	int fd = open("test.txt" , O_RDONLY);
-	if (fd == -1)
-		return (1);
-	
-	char *line;
-	while ((line = get_next_line(fd)) != NULL)
-	{
-		printf("%s" , line);
-		free(line);
-	}
-	return(0);
+	return (free(tmp_buffer) , extract_line(&bank));
 }
